a766cde340e16a86a014c11eb718be6e01c5c1c5
Dmitriy <mityzykin@yandex.ru>
2024-09-04 20:39:59 +0300

    initial commit

--- Probabilities for Sums in Rolling Cubic Dice.py
+++ Probabilities for Sums in Rolling Cubic Dice.py
@@ -1,4 +1,4 @@
-'''Probabilities for Sums in Rolling Cubic Dice'''
+'''Probabilities for Sums in Rolling Cubic Dice with 6 Faces'''
 
 def rolldice_sum_prob(sum_, dice_amount):
 
@@ -6,30 +6,32 @@ def rolldice_sum_prob(sum_, dice_amount):
         return 0
     else:
 
-        mat_s = []
-        c = [1, 2, 3, 4, 5, 6]
-        s = [1, 1, 1, 1, 1, 1]
-        k = 1
+        matrix_numbers = []
+        list_numbers = [1, 2, 3, 4, 5, 6]
+        list_sum = [1, 1, 1, 1, 1, 1]
+        count_cube = 1
 
-        while k < dice_amount:
-            mat_s.clear()
-            mat_s.append(s + [0] + [0] + [0] + [0] + [0])
-            mat_s.append([0] + s + [0] + [0] + [0] + [0])
-            mat_s.append([0] + [0] + s + [0] + [0] + [0])
-            mat_s.append([0] + [0] + [0] + s + [0] + [0])
-            mat_s.append([0] + [0] + [0] + [0] + s + [0])
-            mat_s.append([0] + [0] + [0] + [0] + [0] + s)
-            s.clear()
-            for i in range(len(mat_s[0])):
+        while count_cube < dice_amount:
+            matrix_numbers.clear()
+            matrix_numbers.append(list_sum + [0] + [0] + [0] + [0] + [0])
+            matrix_numbers.append([0] + list_sum + [0] + [0] + [0] + [0])
+            matrix_numbers.append([0] + [0] + list_sum + [0] + [0] + [0])
+            matrix_numbers.append([0] + [0] + [0] + list_sum + [0] + [0])
+            matrix_numbers.append([0] + [0] + [0] + [0] + list_sum + [0])
+            matrix_numbers.append([0] + [0] + [0] + [0] + [0] + list_sum)
+            list_sum.clear()
+            for i in range(len(matrix_numbers[0])):
                 summ = 0
                 for j in range(6):
-                    summ += mat_s[j][i]
-                s.append(summ)
-            k += 1
-            fist_num_c = c[0]
-            c.clear()
-            for h in range(fist_num_c + 1, k * 6 + 1):
-                c.append(h)
+                    summ += matrix_numbers[j][i]
+                list_sum.append(summ)
+            count_cube += 1
+            fist_num_c = list_numbers[0]
+            list_numbers.clear()
+            for h in range(fist_num_c + 1, count_cube * 6 + 1):
+                list_numbers.append(h)
 
-        idx = c.index(sum_)
-        rez = s[idx] / 6 ** dice_amount
+        idx = list_numbers.index(sum_)
+        rez = list_sum[idx] / 6 ** dice_amount
+
+        return rez
