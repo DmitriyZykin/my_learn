81161ae1f55e5e82bb5b5a2f83430169e822ca22
Dmitriy <mityzykin@yandex.ru>
2024-11-08 22:51:20 +0300

    initial commit

--- quick sort.py
+++ quick sort.py
@@ -3,10 +3,9 @@
 def quick_sort(arr):
     if len(arr) < 2:
         return arr
-    else:
-        pivot = arr[0]
-        less = [i for i in arr[1:] if i < pivot]
-        greater = [i for i in arr[1:] if i > pivot]
+    pivot = arr[0]
+    less = [i for i in arr[1:] if i < pivot]
+    greater = [i for i in arr[1:] if i >= pivot]
     return quick_sort(less) + [pivot] + quick_sort(greater)
 
-print(quick_sort([9,4,8,6,1,3,7]))
+print(quick_sort([9,4,3,8,6,1,3,7]))
